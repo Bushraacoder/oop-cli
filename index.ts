4b3d624385184bcfa64ed7f2ca83d251a71ffd3c
Bushra Asim <160628669+Bushraacoder@users.noreply.github.com>
2024-07-22 14:40:06 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,57 @@
+import inquirer from "inquirer";
+
+class Student {
+    name: string
+    constructor(n: string) {
+        this.name = n
+    }
+}
+
+class Person {
+    students: Student[] = []
+    addStudent(obj: Student) {
+        this.students.push(obj)
+    }
+}
+
+const persons = new Person()
+
+const programStart = async (persons: Person) => {
+    do {
+        console.log("WELCOME")
+        const ans = await inquirer.prompt({
+            name: "Select",
+            type: "list",
+            message: "Whom would you like to interact with?",
+            choices: ["staff", "student", "exit"]
+        })
+        
+        if (ans.Select == "staff") {
+            console.log("You have reached the staff room. You can ask any question!")
+        } else if (ans.Select == "student") {
+            const studentAns = await inquirer.prompt({
+                name: "student",
+                type: "input",
+                message: "Enter the student name you wish to contact with:"
+            })
+            const student = persons.students.find(val => val.name == studentAns.student)
+            if (!student) {
+                const newStudent = new Student(studentAns.student)
+                persons.addStudent(newStudent)
+                console.log(`Hello, I am ${newStudent.name}. Nice to meet you!`)
+                console.log("New student added")
+                console.log("Current student list:")
+                console.log(persons.students)
+            } else {
+                console.log(`Hello, I am ${student.name}. Nice to see you again!`)
+                console.log("Existing student list:")
+                console.log(persons.students)
+            }
+        } else if (ans.Select == "exit") {
+            console.log("Exiting program...")
+            break
+        }
+    } while (true)
+}
+
+programStart(persons)
